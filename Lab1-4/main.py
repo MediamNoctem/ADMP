ab2cc41c89c78168ca71f80d6ecce290cc0358b9
MediamNoctem <romAnastasia79@yandex.ru>
2022-10-24 01:27:42 +0300

    Add Lab2 (ex1-3)

--- Lab1-4/main.py
+++ Lab1-4/main.py
@@ -83,32 +83,29 @@ def read_ip_write_to_file():
     cv2.destroyAllWindows()
 
 
+# Создание матрицы свертки
 def convolution_matrix(size, sigma, a, b):
     gauss = [[0.0 for i in range(size)] for j in range(size)]
     for i in range(size):
         for j in range(size):
-            gauss[i][j] = (1 / (2 * math.pi * (sigma ** 2))) * math.exp(-(((i - a) ** 2 + (j - b) ** 2) / (2 * (sigma ** 2))))
+            gauss[i][j] = (1 / (2 * math.pi * (sigma ** 2))) * math.exp(
+                -(((i - a) ** 2 + (j - b) ** 2) / (2 * (sigma ** 2))))
+    sum = np.sum(gauss)
+    gauss /= sum
     return gauss
 
 
-def mult_elem_by_elem(matrix1, matrix2):
-    size = len(matrix1)
-    mult = [[0.0 for i in range(size)] for j in range(size)]
-    for i in range(size):
-        for j in range(size):
-            mult[i][j] = matrix1[i][j] * matrix2[i][j]
-    return mult
-
-
+# Умножение для размытия по Гауссу
 def mult_for_gauss_blur(img, conv_matrix):
     n = len(conv_matrix)
     margin = n // 2
     for i in range(len(img) - margin * 2):
         for j in range(len(img[0]) - margin * 2):
-            img[margin + i][margin + j] = np.sum(mult_elem_by_elem(img[i:i + n, j:j + n], conv_matrix))
+            img[margin + i][margin + j] = np.sum(np.multiply(img[i:i + n, j:j + n], conv_matrix))
     return img
 
 
+# Реализация размытия по Гауссу
 def gaussian_blur(size_conv_matrix, sigma, a, b, path):
     conv_matrix = convolution_matrix(size_conv_matrix, sigma, a, b)
     gray_image = cv2.imread(path, 0)
@@ -140,3 +137,19 @@ if __name__ == '__main__':
     # 6
     """read_ip_write_to_file()
     """
+
+    # lab2
+
+    # lab2
+    # view_image(r'C:\\Users\\romAn\\image\\husky.jpg')
+    conv_matrix_size = 21
+    sigma = 0.3 * ((conv_matrix_size - 1) * 0.5 - 1) + 0.8
+    gaussian_blur(conv_matrix_size, sigma, (conv_matrix_size - 1) // 2, (conv_matrix_size - 1) // 2, r'C:\\Users\\romAn\\image\\husky.jpg')
+    """img = cv2.imread(r'C:\\Users\\romAn\\image\\husky.jpg', 0)
+                img_blur = cv2.blur(img, (20, 20))
+                cv2.namedWindow("Blur")
+                cv2.imshow("Blur", img_blur)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+    """
+    
